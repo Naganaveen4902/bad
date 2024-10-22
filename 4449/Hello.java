f8e2b57fc5af6b9bc6caee4b65b790bc09d23cba
Naganaveen4902 <masanamnaveen88@gmail.com>
2024-10-22 16:02:51 +0530

    Update Hello.java

--- 4449/Hello.java
+++ 4449/Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
  public static void main(String args[])
  {
-   System.out.println("Remove the fake person in you life);
+   System.out.println("Remove the fake person in you life");
  }
 }
